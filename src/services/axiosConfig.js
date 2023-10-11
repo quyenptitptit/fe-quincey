34b151c400344d35ec77e46f996bfaf2b31eecaf
quyenptitptit <quyenmeomeo111@gmail.com>
2023-10-12 00:10:58 +0700

    fix: url

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const axiosConfig = axios.create({
-  // baseURL: "https://be-quincey.vercel.app/api/v1",
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "https://be-quincey.vercel.app/api/v1",
+  // baseURL: "http://localhost:8080/api/v1",
   headers: {
-    "Access-Control-Allow-Origin": "*"
-  }
+    "Access-Control-Allow-Origin": "*",
+  },
 });
 
 axiosConfig.interceptors.request.use(async (config) => config);
